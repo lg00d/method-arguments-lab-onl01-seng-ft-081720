a7bbab488494bbb38e559cf931a3d640537d7600
elizabeth goodman <egoodman1180@icloud.com>
2020-08-18 18:03:50 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,6 +8,6 @@ def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language(Liz,Ruby)
-  puts "Hi, my name is #{Liz} and I am learning to program in #{Ruby}."
+def introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
