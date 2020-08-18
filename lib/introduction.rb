cedb3ff37eb8ba500ca52c88c408bf5aea119833
elizabeth goodman <egoodman1180@icloud.com>
2020-08-18 17:59:33 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,9 @@
 # lib/introduction.rb
 
 def introduction(name)
-  puts "Hi, my name is #{liz} and I am learning to program in #{Ruby}."
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
